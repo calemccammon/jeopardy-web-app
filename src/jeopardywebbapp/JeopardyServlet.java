1a2478dcc147a3f120cd9ae60d20e474ca46655e
calemccammon <calemccammon@gmail.com>
2019-01-26 12:44:26 -0500

    gitignore fixes

--- src/jeopardywebbapp/JeopardyServlet.java
+++ src/jeopardywebbapp/JeopardyServlet.java
@@ -18,7 +18,7 @@ public class JeopardyServlet extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
     public JeopardyServlet() {
-        super();
+        super();dsds
         // TODO Auto-generated constructor stub
     }
 
