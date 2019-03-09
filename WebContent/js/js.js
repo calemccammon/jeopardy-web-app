cbda571edbd8dfc559c0712ca33b2088fcd678e0
calemccammon <calemccammon@gmail.com>
2019-03-09 14:10:10 -0500

    removed hover check

--- WebContent/js/js.js
+++ WebContent/js/js.js
@@ -214,7 +214,6 @@ $("#next-button").click(function() {
 	if($(".lds-ring").is(":hidden")) {
 		loadClue();
 	}
-	$("#next-button").off('hover');
 });
 
 // Update score modal with current score data
